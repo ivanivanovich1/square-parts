1bd9274eaefb3adb13548a4c215f861c52e86c36
John Xina <106729343+ivanivanovich1@users.noreply.github.com>
2023-02-22 11:54:06 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,38 @@
+"use strict";
+
+const csvjson = require("csvjson");
 const express = require("express");
+
+const fs = require("fs");
+const path = require("path");
+
+const data = fs.readFileSync(path.join(__dirname, "../data/LE.txt"), {
+  encoding: "utf8",
+});
+
+const options = {
+  delimiter: "\t",
+  quote: '"',
+};
+
+const parts = csvjson.toObject(data, options);
+
 const app = express();
 const port = 3000;
 
+app.set("json spaces", 40);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("/parts");
+});
+
+// todo: pagination (30 parts per one page)
+app.get("/parts", (req, res) => {
+  res.json(parts);
+});
+
+app.get("/parts/:serial", (req, res) => {
+  res.json(parts.find((e) => e["serial"] === req.params.serial));
 });
 
 app.listen(port, () => {
